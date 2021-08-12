cc568ef11ed441df829d00ffa37d80be01faea06
Peter Gedeck <peter.gedeck@gmail.com>
2021-08-12 15:50:33 -0400

    Boston Housing

--- mlba/data-raw/0-prepareDataFiles.R
+++ mlba/data-raw/0-prepareDataFiles.R
@@ -5,5 +5,8 @@ library(devtools)
 accidents = read.csv(file.path('data-raw', 'accidents.csv.gz'))
 use_data(accidents, overwrite=TRUE) #, file=file.path('data', 'accidents.rda'))
 
+BostonHousing = read.csv(file.path('data-raw', 'BostonHousing.csv.gz'))
+use_data(BostonHousing, overwrite=TRUE) # , file=file.path('data', 'BostonHousing.rda'))
+
 WestRoxbury = read.csv(file.path('data-raw', 'WestRoxbury.csv.gz'))
 use_data(WestRoxbury, overwrite=TRUE) # , file=file.path('data', 'WestRoxbury.rda'))
